438b60954b5e34463bb7a2dc52343e40b8981a65
Lifaqiang <576245069@qq.com>
2018-08-11 20:50:55 +0800

    this is first file.

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 3.cpp
+	> Author: Lifaqiang
+	> Mail: 576245069@qq.com 
+	> Created Time: 六  8/11 20:49:56 2018
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main() {
+    printf("I love China!");
+    return 0;
+} 
