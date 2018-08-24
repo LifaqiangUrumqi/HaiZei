6e37997bf2e90b1a0e4f06f9796c2f7a7c11b77e
Lifaqiang <576245069@qq.com>
2018-08-24 20:39:22 +0800

    I commit a lot of files or directories.

---
+++ Linux_and_Shell/getSpace.sh
@@ -0,0 +1,21 @@
+#########################################################################
+# File Name: getSpace.sh
+# Author: Lifaqiang
+# mail: 576245059@qq.com
+# Created Time: 五  8/24 20:38:52 2018
+#########################################################################
+
+#!/bin/bash
+
+if [[ -n "$1" ]]; then
+    PreAvg=$1
+else
+    PreAvg=100
+fi
+
+Space=`free -m | head -n 2 | tail -n 1 | awk '{printf("%s %s", $2, $4)}'`
+Space=(`echo $Space`)
+Occupy=`echo "scale=1;(${Space[0]}-${Space[1]})*100/${Space[0]}" | bc`
+NowAvg=`echo "scale=1; 0.3*${PreAvg}+0.7*${Occupy}" | bc`
+NowTime=`date +"%Y-%m-%d_%H:%M:%S"`
+echo "$NowTime ${Space[0]}M ${Space[1]}M ${Occupy}% ${NowAvg}%"
