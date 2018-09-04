30e4fc9b09249bfd7156fb4c4ae8036b0250c185
Lifaqiang <576245069@qq.com>
2018-09-04 21:10:23 +0800

    I commit a lot of files or directories.

--- EulerProject/EP_43/EP_43_1.cpp
+++ EulerProject/EP_43/EP_43_1.cpp
@@ -5,6 +5,24 @@
 	> Created Time: 二  9/ 4 17:58:57 2018
  ************************************************************************/
 
+/*
+ * Problem 43
+ * Sub-string divisibility
+ *The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+
+ *Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+
+ *d2d3d4=406 is divisible by 2
+ *d3d4d5=063 is divisible by 3
+ *d4d5d6=635 is divisible by 5
+ *d5d6d7=357 is divisible by 7
+ *d6d7d8=572 is divisible by 11
+ *d7d8d9=728 is divisible by 13
+ *d8d9d10=289 is divisible by 17
+ *Find the sum of all 0 to 9 pandigital numbers with this property.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,7 +36,7 @@ char get_numstr[10000][11];
 int get_sum[20] = {1, 0};
 int n = 0;
 
-void Init_Prime() {
+void Init_Prime() {//线性筛
     is_not_prime[0] = is_not_prime[1] = 1;
     for (int i = 2; i <= MAX_N; i++) {
         if (!is_not_prime[i]) prime[++prime[0]] = i;
@@ -31,7 +49,7 @@ void Init_Prime() {
     return ;
 }
 
-void BigInteger_Addition(char getstr[10000][11]) {
+void BigInteger_Addition(char getstr[10000][11]) {//运用大整数加法来求和
     for (int i = 0; i < n; i++) {
         printf("%s\n", getstr[i]);
         int len = strlen(getstr[i]);
@@ -54,7 +72,7 @@ void BigInteger_Addition(char getstr[10000][11]) {
     return ;
 }
 
-void Is_Divisibility_Property(char numstr[]) {
+void Is_Divisibility_Property(char numstr[]) {//判断是是否是符合要求的数
     int flag = 0;
     for (int i = 1; i + 2 < strlen(numstr); i++) {
         int num = (numstr[i] - '0') * 100 + (numstr[i + 1] - '0') * 10 + (numstr[i + 2] - '0');
@@ -72,18 +90,16 @@ void Is_Divisibility_Property(char numstr[]) {
     return ;
 }
 
-void Swap(char *a, char *b) {
+void Swap(char *a, char *b) {//交换字符
     char tmp = *a;
     *a = *b;
     *b = tmp;
     return ;
 }
 
-void Full_Permutation(char numstr[], int k, int m) {
+void Full_Permutation(char numstr[], int k, int m) {//用递归来进行全排列
     if (k == m) {
-        //printf("%s", numstr);
-        //printf("\n");
-        Is_Divisibility_Property(numstr);
+        Is_Divisibility_Property(numstr);//找出符合要求的数
     } else {
         for (int i = k; i <= m; i++) {
             Swap(&numstr[k], &numstr[i]);
@@ -95,9 +111,8 @@ void Full_Permutation(char numstr[], int k, int m) {
 }
 
 int main() {
-    Init_Prime();
-    Full_Permutation(numstr, 0, 9);
-    BigInteger_Addition(get_numstr);
-    //for (int i = 1; i <= 100; i++) printf("%d\n", prime[i]);
+    Init_Prime();//初始化线性筛
+    Full_Permutation(numstr, 0, 9);//0到9进行全排列
+    BigInteger_Addition(get_numstr);//运用大整数加法来算出最终的和
     return 0;
 }
